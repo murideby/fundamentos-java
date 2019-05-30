a3ff66675ccccf2fc1427b11837cf1b248c4620a
debora.mssilva <debora.mssilva@TAT0541261W10-1.senacsp.edu.br>
2019-05-29 21:20:15 -0300

    envio da classe PDV

---
+++ Fundamentos/src/PDV.java
@@ -0,0 +1,14 @@
+import java.text.DateFormat;
+import java.util.Date;
+
+public class PDV {
+
+	public static void main(String[] args) {
+		//Date -> trabalhar com Data
+		Date data = new Date();
+		//Formatador a data
+		DateFormat formatador = DateFormat.getDateInstance(DateFormat.FULL);
+		System.out.println(formatador.format(data));
+	}
+
+}
